dfc16973c9da1c7211365ec4178fec38893c6fc7
eliezede <99428377+eliezede@users.noreply.github.com>
2025-12-07 08:20:13 +0000

    Update firebase.ts

--- services/firebase.ts
+++ services/firebase.ts
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCRSmO_LiqNYLfy7MPXFQxQs4Xm6TIE4ok",
-  authDomain: "u-fi-72fa4.firebaseapp.com",
-  projectId: "u-fi-72fa4",
-  storageBucket: "u-fi-72fa4.firebasestorage.app",
-  messagingSenderId: "684875105650",
-  appId: "1:684875105650:web:bd33835568f22ad6d189eb",
-  measurementId: "G-Q459YL76E9"
+  
 };
 
 const app = initializeApp(firebaseConfig);
